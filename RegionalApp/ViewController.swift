7f87ac57ae78c0d41035a04ecdeca6f35ff081e4
Vineet Kumar <vic.pavlick@amanomcgann.com>
2024-12-30 14:44:55 -0600

    initial commit

---
+++ RegionalApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RegionalApp
+//
+//  Created by Event HHU on 12/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
